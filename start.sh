2d480045963734c3f34ac4d3348ab7b459f04abb
Tom Burdick <tburdick@wrightwoodtech.com>
2011-10-12 10:30:01 -0500

    fix startup script

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd `dirname $0`
-erl +P 1000000 -smp -sname myapp -setcookie myapp -pa $PWD/ebin $PWD/apps/*/ebin $PWD/deps/*/ebin +Ktrue +A32 +S8 -boot start_sasl -s myapp 
+erl +P 1000000 -smp -sname erlapp -setcookie erlapp -pa $PWD/ebin $PWD/apps/*/ebin $PWD/deps/*/ebin +Ktrue +A32 +S8 -boot start_sasl -s erlapp
